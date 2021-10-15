c46ec8cf5cc6bc90df262490214b1ec2afa0f88f
Han Hossain <ahanuf12@gmail.com>
2021-10-14 18:06:18 -0700

    create LED wrapper

---
+++ src/led.rs
@@ -0,0 +1,43 @@
+use nrf52840_hal::gpio::p0::{P0_29, P0_30, P0_31};
+use nrf52840_hal::gpio::{Level, Output, Pin, PushPull};
+use nrf52840_hal::prelude::*;
+
+/// LED wrapper around a pin
+pub struct Led {
+    pin: Pin<Output<PushPull>>,
+}
+
+impl Led {
+    /// Wraps the pin in the LED wrapper
+    fn new(pin: Pin<Output<PushPull>>) -> Led {
+        Led { pin }
+    }
+
+    /// Creates the red LED (LED1, p0.30)
+    pub fn red<T>(pin: P0_30<T>) -> Led {
+        let pin = pin.degrade().into_push_pull_output(Level::High);
+        Led { pin }
+    }
+
+    /// Creates the green LED (LED2, p0.29)
+    pub fn green<T>(pin: P0_29<T>) -> Led {
+        let pin = pin.degrade().into_push_pull_output(Level::High);
+        Led { pin }
+    }
+
+    /// Creates the blue LED (LED3, p0.31)
+    pub fn blue<T>(pin: P0_31<T>) -> Led {
+        let pin = pin.degrade().into_push_pull_output(Level::High);
+        Led { pin }
+    }
+
+    /// Turns the LED on
+    pub fn on(&mut self) {
+        self.pin.set_low().unwrap();
+    }
+
+    /// Turns the LED off
+    pub fn off(&mut self) {
+        self.pin.set_high().unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 #![no_std]
 #![no_main]
 
+mod leds;
 mod logger;
 
 // pull in the memory layout
 use nrf52840_hal as _;
 
+use crate::leds::Led;
 use cortex_m::asm;
 use cortex_m_rt::entry;
 use log::LevelFilter;
@@ -22,21 +24,41 @@ fn main() -> ! {
     let peripherals = Peripherals::take().unwrap();
 
     let pins = p0::Parts::new(peripherals.P0);
-    let mut led = pins.p0_30.degrade().into_push_pull_output(Level::High);
+    let mut red = Led::red(pins.p0_30);
+    let mut green = Led::green(pins.p0_29);
+    let mut blue = Led::blue(pins.p0_31);
 
-    log::debug!("Pin P0-30 configured for output (red led)");
+    log::debug!("Configured LEDs");
 
     let core_peripherals = CorePeripherals::take().unwrap();
     let mut delay = Delay::new(core_peripherals.SYST);
 
-    loop {
-        // blink led1 (red), p0.30
-        log::debug!("Led on");
-        led.set_low().unwrap();
-        delay.delay_ms(1000u16);
+    // blink led1 (red), p0.30
+    red.on();
+    sleep(&mut delay);
 
-        log::debug!("Led off");
-        led.set_high().unwrap();
-        delay.delay_ms(1000u16);
+    red.off();
+    sleep(&mut delay);
+
+    // blink led2 (green), p0.29
+    green.on();
+    sleep(&mut delay);
+
+    green.off();
+    sleep(&mut delay);
+
+    // blink led3 (blue), p0.31
+    blue.on();
+    sleep(&mut delay);
+
+    blue.off();
+    sleep(&mut delay);
+
+    loop {
+        asm::bkpt();
     }
+}
+
+fn sleep(delay: &mut Delay) {
+    delay.delay_ms(1000u16);
 }
